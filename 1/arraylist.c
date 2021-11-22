46863145bf3ad27ad31948b4cfef080006f72a4b
Yeeun Ju <yeju@c8r2s2.42seoul.kr>
2021-11-22 19:02:15 +0900

    4

--- 1/arraylist.c
+++ 1/arraylist.c
@@ -3,18 +3,6 @@
 #include <stdlib.h> //malloc
 #include <stdio.h> //printf
 
-typedef struct ArrayListNodeType
-{
-	int data;
-} ArrayListNode;
-
-typedef struct ArrayListType
-{
-	int maxElementCount;		// 최대 원소 개수
-	int currentElementCount;	// 현재 원소의 개수
-	ArrayListNode *pElement;	// 원소 저장을 위한 1차원 배열
-} ArrayList;
-
 ArrayList* createArrayList(int maxElementCount)
 {	
 	ArrayList *array;
