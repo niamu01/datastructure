38f5775bace90c74e63bb70ddb628f6f65c7fde4
Yeeun Ju <yeju@c8r7s6.42seoul.kr>
2021-11-25 18:21:50 +0900

    1

---
+++ 2/reverseLinkedList.c
@@ -0,0 +1,15 @@
+void reverseLinkedList(LinkedList* pList) 
+{
+    ListNode *pNode = NULL, *pCurrentNode = NULL, *pPrevNode = NULL;
+    if (pList != NULL) 
+    {
+        pNode = pList->headerNode.pLink;
+        while(pNode != NULL) 
+        {
+            pPrevNode = pCurrentNode;
+            pCurrentNode = pNode;
+            pNode = pNode->pLink; pCurrentNode->pLink = pPrevNode;
+        }
+    pList->headerNode.pLink = pCurrentNode;
+    }
+}
